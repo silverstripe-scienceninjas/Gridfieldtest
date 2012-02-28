bae8f21df5ad1617661d1f60ab9e93625e2f5708
Ingo Schommer <ingo@silverstripe.com>
2012-02-28 20:56:01 +0100

    Updated CompaniesPage to match new GridFieldPopupForms API

--- CompaniesPage.php
+++ CompaniesPage.php
@@ -19,6 +19,25 @@ class CompaniesPage extends Page {
 		$page->write();
 		$page->doPublish();
 	}
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$config = new GridFieldConfig();
+		$config->addComponent(new GridFieldDefaultColumns());
+		$config->addComponent(new GridFieldSortableHeader());
+		$config->addComponent(new GridFieldPaginator);
+		$config->addComponent(new GridFieldFilter());
+		$config->addComponent(new GridFieldAction_Delete());
+		$config->addComponent(new GridFieldAction_Edit());
+		$config->addComponent($forms = new GridFieldPopupForms());
+		
+		$grid = new GridField('Companies', 'Companies', new DataList('Company'),$config);
+
+		$fields->addFieldToTab('Root.Main', $grid);
+
+		return $fields;
+	}
 }
 
 /**
@@ -55,68 +74,5 @@ class CompaniesPage_Controller extends Page_Controller {
 		
 		$grid = new GridField('Companies', 'Companies', new DataList('Company'),$config);
 		return new Form($this,'GridForm',new FieldList($grid),new FieldList());
-	}
-}
-
-/**
- * This class is an GridField Component that add Delete action for Objects in the GridField
- * 
- */
-class CompaniesPage_ViewAction implements GridField_ColumnProvider {
-	
-	/**
-	 * Add a column 'Delete'
-	 * 
-	 * @param type $gridField
-	 * @param array $columns 
-	 */
-	public function augmentColumns($gridField, &$columns) {
-		$columns[] = 'EditAction';
-	}
-	
-	/**
-	 * Return any special attributes that will be used for FormField::createTag()
-	 *
-	 * @param GridField $gridField
-	 * @param DataObject $record
-	 * @param string $columnName
-	 * @return array
-	 */
-	public function getColumnAttributes($gridField, $record, $columnName) {
-		return array();
-	}
-	
-	/**
-	 * Add the title 
-	 * 
-	 * @param GridField $gridField
-	 * @param string $columnName
-	 * @return array
-	 */
-	public function getColumnMetadata($gridField, $columnName) {
-		if($columnName == 'EditAction') {
-			return array('title' => 'Edit');
-		}
-	}
-	
-	/**
-	 * Which columns are handled by this component
-	 * 
-	 * @param type $gridField
-	 * @return type 
-	 */
-	public function getColumnsHandled($gridField) {
-		return array('EditAction');
-	}
-	
-	/**
-	 *
-	 * @param GridField $gridField
-	 * @param DataObject $record
-	 * @param string $columnName
-	 * @return string - the HTML for the column 
-	 */
-	public function getColumnContent($gridField, $record, $columnName) {
-		return sprintf('<a class="action-edit" href="%s">%s</a>', Controller::join_links($gridField->Link('item'), $record->ID, 'edit'), _t('CompanyPage.Edit', 'Edit'));
 	}
 }
